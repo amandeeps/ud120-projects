c225dc0cf617bcb447401c3949bf02d568fde46d
Amandeep Singh <amandeeps@outlook.in>
2016-12-24 15:42:58 +0530

    Classify Email using Support Vector Machines

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -13,17 +13,28 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+
+from sklearn.svm import SVC
+clf = SVC(kernel = 'rbf', C=10000)
+
+t0 = time()
+print "Training Start."
+clf.fit(features_train, labels_train)
+print "Training Time: ", round(time() - t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "Prediction Time: ", round(time() - t1, 3), "s"
+
+from sklearn.metrics import accuracy_score
+print "Accuracy: ", accuracy_score(labels_test, pred)
 
 #########################################################
 
